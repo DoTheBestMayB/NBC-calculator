a6764f8a5e8fde08b86bbf865c3c782e3d3a8694
DoDoBest <send1684@gmail.com>
2024-03-07 19:42:06 +0900

    test: Calculator 클래스의 단일 연산식 테스트 코드 작성

--- src/test/kotlin/domain/CalculatorTest.kt
+++ src/test/kotlin/domain/CalculatorTest.kt
@@ -3,6 +3,7 @@ package domain
 import domain.operation.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 class CalculatorTest {
@@ -18,6 +19,76 @@ class CalculatorTest {
             divideOperation = DivideOperation(),
             remainderOperation = RemainderOperation()
         )
+    }
+
+    @DisplayName("20+30을 입력하면 50을 반환한다")
+    @Test
+    fun `add solo test`() {
+        // given
+        val input = "20+30"
+
+        // when
+        val actual = calculator.calculate(input)
+
+        // then
+        val expected = 50.0
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @DisplayName("200-312을 입력하면 -112을 반환한다")
+    @Test
+    fun `subtract solo test`() {
+        // given
+        val input = "200-312"
+
+        // when
+        val actual = calculator.calculate(input)
+
+        // then
+        val expected = -112.0
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @DisplayName("20*3을 입력하면 60을 반환한다")
+    @Test
+    fun `multiply solo test`() {
+        // given
+        val input = "20*3"
+
+        // when
+        val actual = calculator.calculate(input)
+
+        // then
+        val expected = 60.0
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @DisplayName("21/4를 입력하면 5.25를 반환한다")
+    @Test
+    fun `divide solo test`() {
+        // given
+        val input = "21/4"
+
+        // when
+        val actual = calculator.calculate(input)
+
+        // then
+        val expected = 5.25
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @DisplayName("26%7을 입력하면 5를 반환한다")
+    @Test
+    fun `modulo solo test`() {
+        // given
+        val input = "26%7"
+
+        // when
+        val actual = calculator.calculate(input)
+
+        // then
+        val expected = 5.0
+        assertThat(actual).isEqualTo(expected)
     }
 
     @Test
